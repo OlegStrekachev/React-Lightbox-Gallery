38eb6a7f9d80652dcb9f746045cebed91ed9fd34
Oleg Strekachev <olegstrekachev@gmail.com>
2024-01-08 18:18:29 -0700

    TranslateX dragging setup (pre-messed up)

--- src/features/gallery/components/GalleryContainer.tsx
+++ src/features/gallery/components/GalleryContainer.tsx
@@ -19,118 +19,63 @@ const moduleFiles = import.meta.glob("../assets/*.jpg", {
 const imageModules: ImageModule[] = Object.values(moduleFiles);
 
 export const GalleryContainer = () => {
-  const [initiatedInteraction, setInitiatedInteraction] = useState(false);
-  const [isDragging, setIsDragging] = useState(false);
-  const [dragStartX, setDragStartX] = useState(0);
-  const [scrollLeftStart, setScrollLeftStart] = useState<number>(0);
+  const [mouseWasDown, setMouseWasDown] = useState(false);
 
+  const [moveStartX, setMoveStartX] = useState<number>(0);
+
+  const [isDragging, setIsDragging] = useState(false);
+  const [currentTranslateX, setCurrentTranslateX] = useState<number>(0);
   const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+  const [dragMovementDistance, setDragMovementDistance] = useState<number>(0);
 
   const imageRefs = useRef(new Map<number, HTMLImageElement>()).current;
   const reverseImageRefs = useRef(new Map<HTMLImageElement, number>()).current;
-
   const galleryContainerRef = useRef<HTMLDivElement>(null);
-
-  // function to scroll to the image with the given id
-  function scrollToId(imageNode: HTMLImageElement) {
-    imageNode.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "center",
-    });
-  }
 
   //  function to determine the index of the closest gallery image to the center of the screen (for now let's assume the center of the screen is the center of the gallery container)
 
-  const findClosestImageIndex = (): number | null => {
-    // Assign to a local variable to ensure non-null value within the loop
-    const galleryCurrent = galleryContainerRef.current;
-
-    // Early return if galleryCurrent is null
-    if (!galleryCurrent) return null;
-
-    let closestDistance = Infinity;
-    let closestImageIndex: number | null = null;
-
-    imageRefs.forEach((imageNode) => {
-      const imgRectModel = imageNode.getBoundingClientRect();
-      const imgCenterX = imgRectModel.left + imgRectModel.width / 2;
-
-      const galleryCenter =
-        galleryCurrent.getBoundingClientRect().left +
-        galleryCurrent.clientWidth / 2;
-      const distance = Math.abs(imgCenterX - galleryCenter);
-
-      if (distance < closestDistance) {
-        closestDistance = distance;
-        const index = reverseImageRefs.get(imageNode);
-        closestImageIndex = index !== undefined ? index : null;
-      }
-    });
-
-    return closestImageIndex;
-  };
+  // const findClosestImageIndex = (): number | null => {
+  // };
 
   // defining click and drag events handlers for the gallery container
 
   const onDragStart = (event: React.MouseEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    setInitiatedInteraction(true);
-    setDragStartX(event.clientX);
-    setScrollLeftStart(galleryContainerRef.current!.scrollLeft);
+    // Apply the new transformation
+    if (galleryContainerRef.current) {
+      setMouseWasDown(true);
+      setMoveStartX(event.clientX);
+      const galleryContainerCurrentTranslateX = window
+        .getComputedStyle(galleryContainerRef.current)
+        .transform.split(",")[4];
+      setCurrentTranslateX(Number(galleryContainerCurrentTranslateX));
+      console.log("drag start", galleryContainerCurrentTranslateX);
+    } else {
+      console.log("The element is not rendered yet or the ref is not attached");
+    }
   };
 
   const onDragMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (!galleryContainerRef.current) return;
-    const initialMove = event.clientX - dragStartX;
-    if (initiatedInteraction && Math.abs(initialMove) > 8) {
-      console.log("dragging initiated");
-      setIsDragging(true);
-      galleryContainerRef.current.scrollLeft = scrollLeftStart - initialMove;
+    if (mouseWasDown) {
+      const movementDistance = event.clientX - moveStartX;
+
+      setDragMovementDistance(movementDistance);
+      galleryContainerRef.current.style.transform = `translateX(${
+        currentTranslateX + movementDistance
+      }px)`;
+      console.log("movementDistance", movementDistance);
     }
   };
 
   const onDragEnd = (event: React.MouseEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    if (!galleryContainerRef.current) return;
-    else if (initiatedInteraction && isDragging) {
-      const centeredImageIndex = findClosestImageIndex();
-      if (centeredImageIndex === null) return;
-      setCurrentImageIndex(centeredImageIndex);
-      const centeredImageNode = imageRefs.get(centeredImageIndex);
-      if (centeredImageNode === undefined) return;
-      scrollToId(centeredImageNode);
-    } else if (initiatedInteraction && !isDragging) {
-      console.log("Click is registered");
-      const clickedImageIndex = reverseImageRefs.get(
-        event.target as HTMLImageElement
-      );
-      if (clickedImageIndex === undefined) return;
-      setCurrentImageIndex(clickedImageIndex);
-      scrollToId(event.target as HTMLImageElement);
-    }
-
-    setInitiatedInteraction(false);
-    setIsDragging(false);
+    setCurrentTranslateX(currentTranslateX + dragMovementDistance);
+    setMouseWasDown(false);
   };
 
   // Defining click handlers for the arrow buttons to slide the gallery
 
-  const onSlideRightClick = () => {
-    const nextIndex = currentImageIndex + 1;
-    if (nextIndex < imageModules.length) {
-      setCurrentImageIndex(nextIndex);
-      scrollToId(imageRefs.get(nextIndex));
-    }
-  };
+  const onSlideRightClick = () => {};
 
-  const onSlideLeftClick = () => {
-    const previousIndex = currentImageIndex - 1;
-    if (previousIndex >= 0) {
-      setCurrentImageIndex(previousIndex);
-      scrollToId(imageRefs.get(previousIndex));
-    }
-  };
+  const onSlideLeftClick = () => {};
 
   return (
     <div className={styles.galleryWrapper}>
