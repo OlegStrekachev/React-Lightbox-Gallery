60d63107c88eaa0ff279e1a99b721c4665d20bad
Oleg Strekachev <olegstrekachev@gmail.com>
2024-01-28 20:21:58 -0700

    Stabilized logig - works so far

--- src/features/gallery/components/GalleryContainer.tsx
+++ src/features/gallery/components/GalleryContainer.tsx
@@ -1,7 +1,6 @@
 import styles from "./GalleryContainer.module.css";
 import { useRef, useState, useEffect } from "react";
 import SlideRightIcon from "../assets/vector/slideRightIcon.svg";
-import { current } from "@reduxjs/toolkit";
 
 // Define the type for the imported image modules
 type ImageModule = {
@@ -25,9 +24,10 @@ export const GalleryContainer = () => {
 
   const [mouseWasDown, setMouseWasDown] = useState(false);
   const [moveStartX, setMoveStartX] = useState<number>(0);
-  const [moveEndX, setMoveEndX] = useState<number>(0);
+
   const [isDragging, setIsDragging] = useState(false);
   const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+  const [shouldCenterImage, setShouldCenterImage] = useState(false);
 
   const imageRefs = useRef(new Map<number, HTMLImageElement>()).current;
 
@@ -36,18 +36,19 @@ export const GalleryContainer = () => {
 
   // useEffect to center the image when the drag action is finished and pointer
 
-  // useEffect(() => {
-  //   if (shouldCenterImage && currentImageIndex !== null) {
-  //     const closestImage = imageRefs.get(currentImageIndex);
-  //     if (closestImage && galleryContainerRef.current) {
-  //       scrollImageToCenter(closestImage);
-  //     }
-  //     // Reset the shouldCenterImage state to false to prevent the useEffect from being triggered again
-  //     setShouldCenterImage(false);
-  //   }
-  //   // Disable the exhaustive-deps rule for this useEffect since we want to trigger the useEffect only when the shouldCenterImage state changes
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [shouldCenterImage]);
+  useEffect(() => {
+    if (shouldCenterImage && currentImageIndex !== null) {
+      const closestImage = imageRefs.get(currentImageIndex);
+      if (closestImage && galleryContainerRef.current) {
+        scrollImageToCenter(closestImage);
+        console.log(closestImage);
+      }
+      // Reset the shouldCenterImage state to false to prevent the useEffect from being triggered again
+      setShouldCenterImage(false);
+    }
+    // Disable the exhaustive-deps rule for this useEffect since we want to trigger the useEffect only when the shouldCenterImage state changes
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [shouldCenterImage]);
 
   // Utility function to find the closest image to the viewport center
   const findClosestImageIndex = (): number | null => {
@@ -80,7 +81,13 @@ export const GalleryContainer = () => {
       const imageCenter = imageRect.left + imageRect.width / 2;
       const middleOfTheViewport = window.innerWidth / 2;
       const movementDistance = middleOfTheViewport - imageCenter;
-      const newTranslateX = containerStartX + movementDistance;
+      // Retrieving the current translateX value of the gallery container from the real dom (returnx matrix)
+      const galleryContainerCurrentTranslateX = window
+        .getComputedStyle(galleryContainerRef.current)
+        .transform.split(",")[4];
+      // Updating the translateX value of the gallery container to move it to the center of the viewport
+      const newTranslateX =
+        Number(galleryContainerCurrentTranslateX) + movementDistance;
       galleryContainerRef.current.style.transform = `translateX(${newTranslateX}px)`;
     }
   }; // Add an empty array as the second argument to useCallback
@@ -113,18 +120,17 @@ export const GalleryContainer = () => {
     // Check if the mouse was down before the move event
     if (mouseWasDown) {
       const movementDistance = event.clientX - moveStartX;
-      // Check if any significant movement has been made
-      if (Math.abs(movementDistance) >= 4 && !isDragging) {
-        setIsDragging(true);
-        console.log("Drag action was started");
-        // Initiate tracking index of the image
-        findClosestImageIndex();
-      }
+      findClosestImageIndex();
       // Update the translateX value of the gallery container to move it
       if (galleryContainerRef.current !== null) {
         galleryContainerRef.current.style.transform = `translateX(${
           containerStartX + movementDistance
         }px)`;
+        // Check if any significant movement has been made
+        if (Math.abs(movementDistance) >= 4 && !isDragging) {
+          setIsDragging(true);
+          console.log("Drag action was started");
+        }
       }
     }
   };
@@ -132,18 +138,16 @@ export const GalleryContainer = () => {
   const onPointerUp = (event: React.MouseEvent<HTMLDivElement>) => {
     // Terminate the drag action and calulations of the movement distance when the user releases the mouse button
     setMouseWasDown(false);
-    // Retrieve the current translateX value of the gallery container from the real dom (returnx matrix)
-    setMoveEndX(event.clientX - moveStartX);
-
-    // Differentiate between click and drag actions logic here
+    // IF DRAGGING
     if (isDragging && galleryContainerRef.current !== null) {
+      setIsDragging(false);
       console.log("Drag action was ended");
-      findClosestImageIndex();
-        galleryContainerRef.current.style.transition =
-          "transform 0.2s ease-in-out";
-        console.log("Closest image index is", currentImageIndex);
-        // When pointer is released, below setters will trigger the useEffect to center the image
-        // Empty
+      galleryContainerRef.current.style.transition =
+        "transform 0.2s ease-in-out";
+      console.log("Closest image index is", currentImageIndex);
+      // When pointer is released, below setters will trigger the useEffect to center the image from where the pointer was released
+      setShouldCenterImage(true);
+      // IF CLICKED
     } else if (!isDragging && galleryContainerRef.current !== null) {
       galleryContainerRef.current.style.transition =
         "transform 0.2s ease-in-out";
@@ -162,13 +166,26 @@ export const GalleryContainer = () => {
           // For example, you could set it to a default value or handle the error
           console.log("Clicked image index not found");
         }
-        // You can add more logic here, like identifying which image was clicked
       } else {
         // The click occurred on something else
         console.log("Something else was clicked but image", event.target);
       }
     }
+  };
+
+  const onPointerLeave = () => {
+    // Terminate the drag action and calulations of the movement distance when the pointer leaves the gallery container
+    setMouseWasDown(false);
     setIsDragging(false);
+    console.log("Drag action was ended");
+    if (galleryContainerRef.current !== null) {
+      galleryContainerRef.current.style.transition =
+        "transform 0.2s ease-in-out";
+    }
+    const imageOnPointerLeave = imageRefs.get(currentImageIndex);
+    if (imageOnPointerLeave !== undefined) {
+      scrollImageToCenter(imageOnPointerLeave);
+    }
   };
 
   return (
@@ -195,6 +212,7 @@ export const GalleryContainer = () => {
           onPointerDown={onPointerDown}
           onPointerMove={onPointerMove}
           onPointerUp={onPointerUp}
+          onPointerLeave={onPointerLeave}
         >
           {imageModules.map((imageModule, index: number) => (
             <img
